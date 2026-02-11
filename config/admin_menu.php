ee56a30c55c0d180dbb0b142b42b5cdfee043edd
rine203 <protgas2003@gmail.com>
2026-02-12 04:10:46 +0700

    Cập nhật màn hình admin/new và admin/contact

---
+++ config/admin_menu.php
@@ -0,0 +1,64 @@
+<?php
+
+return [
+    [
+        'label' => 'Manage Products',
+        'icon' => 'fa-box',
+        'url' => '#',
+    ],
+    [
+        'label' => 'Manage Orders',
+        'icon' => 'fa-receipt',
+        'url' => '#',
+    ],
+    [
+        'label' => 'Manage Users',
+        'icon' => 'fa-users',
+        'url' => '#',
+    ],
+    [
+        'label' => 'Manage Promotions',
+        'icon' => 'fa-tag',
+        'url' => '#',
+    ],
+    [
+        'label' => 'Manage Reviews',
+        'icon' => 'fa-star',
+        'url' => '#',
+    ],
+    [
+        'label' => 'Manage Categories',
+        'icon' => 'fa-list',
+        'url' => '#',
+    ],
+    [
+        'label' => 'Manage News',
+        'icon' => 'fa-newspaper',
+        'url' => fn() => route('admin.news.index'),
+    ],
+    [
+        'label' => 'Manage Contacts',
+        'icon' => 'fa-envelope',
+        'url' => fn() => route('admin.contacts.index'),
+    ],
+    [
+        'label' => 'Refunds & Info',
+        'icon' => 'fa-arrow-rotate-left',
+        'url' => '#',
+    ],
+    [
+        'label' => 'Permissions',
+        'icon' => 'fa-lock',
+        'url' => '#',
+    ],
+    [
+        'label' => 'Wallets',
+        'icon' => 'fa-wallet',
+        'url' => '#',
+    ],
+    [
+        'label' => 'Reports & Stats',
+        'icon' => 'fa-chart-bar',
+        'url' => '#',
+    ],
+];
--- database/migrations/2026_02_06_094424_create_contacts_table.php
+++ database/migrations/2026_02_06_094424_create_contacts_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('subject');
+            $table->longText('message');
             $table->timestamps();
         });
     }
